30cecb1eca42a49fa4b2408deeebff7ce506f511
Anillc <i@anillc.cn>
2022-06-17 03:17:20 +0800

    add minimal system

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "Package and module sets for nayuta networking";
+    description = "Package and module sets for nayuta networking";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages = {
-        inherit (pkgs) hello;
-      };
-    });
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system: let
+            pkgs = import nixpkgs {
+                inherit system;
+                overlays = [(self: super: {
+                    inherit nixpkgs;
+                })];
+            };
+        in {
+            packages = {
+                minimal-system = pkgs.callPackage ./minimal-system.nix {};
+            };
+        });
 }
